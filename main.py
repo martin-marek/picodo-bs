0cfc46656e28dd39757191629f6bf559fcfb53c3
Martin Marek <12614710+martin-marek@users.noreply.github.com>
2025-04-16 23:36:34 -0400

    use specialized config resolvers

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import operator as op
 import hydra
 import train
 from omegaconf import OmegaConf, DictConfig
+OmegaConf.register_new_resolver('floordiv', op.floordiv)
+OmegaConf.register_new_resolver('mul', op.mul)
+OmegaConf.register_new_resolver('min', min)
+OmegaConf.register_new_resolver('pow', pow)
+
 
 import os
 os.environ['XLA_FLAGS'] = '--xla_force_host_platform_device_count=8'
@@ -8,7 +14,6 @@ os.environ['XLA_FLAGS'] = '--xla_force_host_platform_device_count=8'
 
 @hydra.main(version_base=None, config_path='configs', config_name='local')
 def main(c: DictConfig):
-    OmegaConf.register_new_resolver('eval', eval)
     train.train_and_evaluate(c)
 
 
