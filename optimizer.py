492a4ee34173a5fe06866f7d464d06d5500124a5
Martin Marek <12614710+martin-marek@users.noreply.github.com>
2025-04-20 13:52:23 -0400

    remove wd mask

--- optimizer.py
+++ optimizer.py
@@ -1,5 +1,4 @@
 import optax
-import jax.tree_util as jtu
 from omegaconf import OmegaConf
 import muon, multistep, utils
 
@@ -37,9 +36,8 @@ def get_optimizer(c: OmegaConf, params, num_microbatch_steps: int, tokens_per_mi
     if c.optimizer == 'adamw':
         assert c.b1 is not None
         assert c.b2 is not None
-        wd_mask = jtu.tree_map_with_path(lambda path, _: path[-2].key=='kernel', params)
         optimizer_factory = optax.inject_hyperparams(multistep_wrapper(optax.adamw, c.grad_acc_steps))
-        optimizer = optimizer_factory(lr_schedule, c.b1, c.b2, weight_decay=c.weight_decay, eps=c.eps, mask=wd_mask)
+        optimizer = optimizer_factory(lr_schedule, c.b1, c.b2, weight_decay=c.weight_decay, eps=c.eps)
     
     if c.optimizer == 'muon':
         assert c.b1 is not None
