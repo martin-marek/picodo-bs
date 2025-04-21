71dc0b4c885432bcd4eb2a172750a6a43510d310
Martin Marek <12614710+martin-marek@users.noreply.github.com>
2025-04-20 23:35:52 -0400

    data: fix train/valid overlap

--- data.py
+++ data.py
@@ -10,7 +10,7 @@ def load_ds(ds_path, seq_len, bs_train, bs_valid, n_tokens_valid, n_tokens_train
     ds_path = os.path.expanduser(ds_path)
     data = np.memmap(ds_path, dtype=np.uint16, mode='r')
     n_tokens_dataset = len(data)
-    n_batch_dataset = n_tokens_dataset // (seq_len+1)
+    n_seq_dataset = n_tokens_dataset // (seq_len+1)
 
     # if n_tokens_train is None, use full dataset
     if n_tokens_train is not None: assert n_tokens_train + n_tokens_valid <= n_tokens_dataset
@@ -24,15 +24,14 @@ def load_ds(ds_path, seq_len, bs_train, bs_valid, n_tokens_valid, n_tokens_train
 
     # sample sequence order
     rng = np.random.default_rng(int(seed))
-    n_seq_dataset = n_tokens_dataset // seq_len
     seq_idx = rng.choice(n_seq_dataset, size=(n_seq_train+n_seq_valid), replace=False)
     seq_idx_train = seq_idx[:n_seq_train].reshape([n_batch_train, bs_train])
-    seq_idx_valid = seq_idx[:n_seq_valid].reshape([n_batch_valid, bs_valid])
+    seq_idx_valid = seq_idx[n_seq_train:].reshape([n_batch_valid, bs_valid])
 
     # define sequence loader
     # using np.memmap for each batch to avoid memory leak
     def get_batch(seq_idxs): # [B]
-        data = np.memmap(ds_path, dtype=np.uint16, shape=[n_batch_dataset, seq_len+1], mode='r')
+        data = np.memmap(ds_path, dtype=np.uint16, shape=[n_seq_dataset, seq_len+1], mode='r')
         return data[seq_idxs] # [B, L]
 
     return get_batch, seq_idx_train, seq_idx_valid
