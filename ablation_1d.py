4890ebe8fc55ef9a1fd5e40fe356bbd576cee299
Martin Marek <12614710+martin-marek@users.noreply.github.com>
2025-05-05 00:00:54 -0400

    add 1d ablation

---
+++ ablation_1d.py
@@ -0,0 +1,24 @@
+import hydra
+import train
+from configs import resolver_setup
+from omegaconf import OmegaConf, DictConfig
+
+
+# load default config
+@hydra.main(version_base=None, config_path='configs')
+def main(c: DictConfig):
+
+    # overwrite batch size params
+    c = OmegaConf.merge(c, c.ablation.bs_config[c.opt.batch_size])
+    del c.ablation.bs_config
+
+    # scaled ablated hparam
+    OmegaConf.update(c, c.ablation.hparam, c.ablation.scaling * OmegaConf.select(c, c.ablation.hparam))
+
+    # run training job
+    print(OmegaConf.to_yaml(c))
+    # train.train_and_evaluate(c)
+
+
+if __name__ == '__main__':
+    main()
