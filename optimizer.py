c24cab15d03cbc1a5328b1d066b5e58155f9d1be
Martin Marek <12614710+martin-marek@users.noreply.github.com>
2025-05-10 11:55:01 -0400

    move muon to optimizer.py

--- optimizer.py
+++ optimizer.py
@@ -1,10 +1,13 @@
+import jax
+import jax.numpy as jnp
 import optax
-from omegaconf import OmegaConf
-from optax._src import transform, combine
-import muon, multistep, utils
+from optax import tree_utils as otu
+from omegaconf import DictConfig
+import multistep, utils
+from typing import NamedTuple
 
 
-def get_optimizer(c: OmegaConf, params, num_microbatch_steps: int, tokens_per_microbatch: int):
+def get_optimizer(c: DictConfig, params, num_microbatch_steps: int, tokens_per_microbatch: int):
     
     # get LR
     assert (c.peak_lr is not None) ^ ((c.peak_lr_scaled is not None) & (c.peak_lr_scaling is not None))
@@ -50,7 +53,87 @@ def get_optimizer(c: OmegaConf, params, num_microbatch_steps: int, tokens_per_mi
         assert c.muon_lr is not None
         assert c.muon_b1 is not None
         muon_lr = optax.schedules.warmup_cosine_decay_schedule(0, c.muon_lr, warmup_steps, num_microbatch_steps)
-        grad_transform = multistep_wrapper(muon.muon, c.grad_acc_steps)
+        grad_transform = multistep_wrapper(muon, c.grad_acc_steps)
         optimizer = optax.inject_hyperparams(grad_transform)(muon_lr, c.muon_b1, lr_schedule, c.b1, c.b2)
 
     return optimizer
+
+
+def orthogonalize_via_newton_schulz(
+    x: jax.Array,
+    ns_coeffs: jax.Array,
+    ns_steps: int = 5,
+    eps: float = 1e-8,
+) -> jax.Array:
+    # https://github.com/google-deepmind/optax/blob/main/optax/contrib/_muon.py 
+    if x.ndim < 2:
+        raise ValueError(f'Input must have >= 2 dims, got {x.shape}')
+    if ns_coeffs.shape != (3,):
+        raise ValueError(f'ns_coeffs must have shape (3,), got {ns_coeffs}')
+    def newton_schulz_iterator(x: jax.Array, coeffs: jax.Array) -> jax.Array:
+        x_mT = jnp.swapaxes(x, -2, -1) # <-- changed (matrix transpose last 2 dims)
+        a = x @ x_mT # <-- changed (use matrix transpose)
+        b = coeffs[1] * a + coeffs[2] * a @ a
+        return coeffs[0] * x + b @ x
+    transposed = False
+    if x.shape[-2] > x.shape[-1]: # <-- changed (check last 2 dims)
+        x = jnp.swapaxes(x, -2, -1) # <-- changed (transpose last 2 dims)
+        transposed = True
+    x /= (jnp.linalg.norm(x, axis=(-2, -1), keepdims=True) + eps) # <-- changed (normalize each matrix slice)
+    ns_coeffs = ns_coeffs.astype(x.dtype)
+    x = jax.lax.fori_loop(0, ns_steps, lambda _, x: newton_schulz_iterator(x, ns_coeffs), x)
+    if transposed: x = jnp.swapaxes(x, -2, -1) # <-- changed (transpose last 2 dims)
+    return x
+
+
+class MuonState(NamedTuple):
+    """State for the Adam algorithm."""
+    count: jax.Array # shape=(), dtype=jnp.int32.
+    mu: optax.Updates
+    ns_coeffs: jax.Array # shape=(), dtype=jnp.int32.
+
+
+def scale_by_muon(
+    ns_coeffs: tuple = (3.4445, -4.7750, 2.0315),
+    ns_steps: int = 5,
+    beta: float = 0.95,
+    eps: float = 1e-8,
+) -> optax.GradientTransformation:
+    # https://github.com/google-deepmind/optax/blob/main/optax/contrib/_muon.py 
+
+    def init_fn(params):
+        mu = otu.tree_zeros_like(params) # First moment
+        return MuonState(jnp.zeros([], jnp.int32), mu, jnp.asarray(ns_coeffs))
+
+    def update_fn(updates, state, params=None):
+        del params
+        mu = otu.tree_update_moment(updates, state.mu, beta, 1)
+        count_inc = optax.safe_increment(state.count)
+        mu_hat = otu.tree_bias_correction(mu, beta, count_inc)
+        # Apply Newton-schulz orthogonalization.
+        updates = jax.tree.map(lambda x: orthogonalize_via_newton_schulz(x, state.ns_coeffs, ns_steps, eps), mu_hat)
+        updates = jax.tree.map(lambda x: jnp.sqrt(jnp.maximum(1, x.shape[-1] / x.shape[-2])) * x, updates)
+        return updates, MuonState(count_inc, mu, state.ns_coeffs)
+    
+    return optax.GradientTransformation(init_fn, update_fn)
+
+
+def muon(
+    learning_rate: float,
+    muon_b1: float,
+    adam_lr: float,
+    adam_b1: float,
+    adam_b2: float,
+) -> optax.GradientTransformation:
+    return optax.multi_transform(
+        transforms={
+            'muon': optax.chain(
+                scale_by_muon(beta=muon_b1),
+                optax.scale_by_learning_rate(learning_rate),
+            ),
+            'adam': optax.adamw(adam_lr, adam_b1, adam_b2)
+        },
+        param_labels=lambda params: jax.tree.map_with_path(
+            lambda path, val: 'adam' if 'embed' in jax.tree_util.keystr(path) else 'muon', params
+        ),
+    )
