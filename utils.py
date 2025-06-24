a3669413219b7ec5f280e86dbc553ff95130894d
Martin Marek <12614710+martin-marek@users.noreply.github.com>
2025-06-24 10:46:50 +0200

    add flatten_dict separator

--- utils.py
+++ utils.py
@@ -4,12 +4,12 @@ from flax import nnx
 from collections.abc import Mapping
 
 
-def flatten_dict(d, prefix=None):
+def flatten_dict(d, prefix=None, sep='.'):
     if isinstance(d, Mapping):
         out = {}
         for k, v in d.items():
-            nested_prefix = k if prefix is None else f'{prefix}.{k}'
-            out |= flatten_dict(v, nested_prefix)
+            nested_prefix = k if prefix is None else f'{prefix}{sep}{k}'
+            out |= flatten_dict(v, nested_prefix, sep)
         return out
     else:
         return {prefix: d}
